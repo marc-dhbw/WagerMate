31a59beb84c40b975808f4d0b66a94259fc56d59
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-08-03 13:45:52 +0200

    fix changed Program.cs to accept env variables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using Npgsql;
 using WagerMate.Components;
 using WagerMate.Data;
 using WagerMate.Services;
@@ -5,11 +7,29 @@ using WagerMate.Services.impl;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add environment variables to the configuration
+builder.Configuration.AddEnvironmentVariables();
+
+// Retrieve the connection string from the environment variable
+var connectionString = Environment.GetEnvironmentVariable("CON_STR");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("The connection string is not set. Please configure the environment variable 'MYAPP_CONNECTIONSTRING'.");
+}
+
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Register the IDbConnection service for Dapper
+builder.Services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(connectionString));
+
+
 builder.Services.AddScoped<IDbService, DbService>();
 builder.Services.AddScoped<IUserService, UserService>();
+
 
 var app = builder.Build();
 
