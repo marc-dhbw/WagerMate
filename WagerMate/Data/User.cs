8833446538e750e06767d9e2afa88e944dca121c
tbehr <->
2024-08-17 13:11:35 +0200

    Added Constructors to User.cs

--- WagerMate/Data/User.cs
+++ WagerMate/Data/User.cs
@@ -6,4 +6,21 @@ public class User
     public string? Email { get; set; }
     public string? Name { get; set; }
     public string? Password { get; set; }
+
+    public User()
+    {
+        Id = 0;
+        Name = "";
+        Email = "";
+        Password = "";
+    }
+
+    public User(int id, string name, string email, string password)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+    
 }
