04078e4082bf9e4e4448bf9c000726ca5d43bac6
anian <anian.seidl@gmail.com>
2024-08-17 12:00:51 +0200

    Added AutoRedirect to Home and Userprofile

--- WagerMate/Services/impl/SessionStorage.cs
+++ WagerMate/Services/impl/SessionStorage.cs
@@ -1,4 +1,5 @@
 using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
 
 namespace WagerMate.Services.impl;
 
@@ -7,5 +8,14 @@ public class SessionStorage : ISessionStorage
     public async Task HandleSession(string key, string data, ISessionStorageService sessionStorage)
     {
         await sessionStorage.SetItemAsync(key, data);
+    }
+
+    public async Task RedirectToLogin(string key, ISessionStorageService sessionStorage, NavigationManager navigation)
+    {
+        var value = await sessionStorage.GetItemAsync<string>(key);
+        if (string.IsNullOrEmpty(value))
+        {
+            navigation.NavigateTo("/login");
+        }
     }
 }
