99e6856f39ef9411be66d8de065bc7f11ca1b325
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-08-20 14:09:19 +0200

    wip hashing

--- WagerMate/Services/impl/HashService.cs
+++ WagerMate/Services/impl/HashService.cs
@@ -8,11 +8,27 @@ namespace WagerMate.Services.impl;
 public class HashService : IHashService
 {
     private readonly IUserService _userService;
+
     public byte[] GenerateSalt()
     {
-        byte[] salt = new byte[16]; 
-        RandomNumberGenerator.Fill(salt); 
+        byte[] salt = new byte[16];
+        RandomNumberGenerator.Fill(salt);
         return salt;
+    }
+
+    public string createhash(string email, string pw)
+    {
+        string inputString = email + pw;
+        StringBuilder sb = new StringBuilder();
+        using (HashAlgorithm algorithm = SHA256.Create())
+        {
+            var x = algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+
+            foreach (byte b in x)
+                sb.Append(b.ToString("X2"));
+        }
+
+        return sb.ToString();
     }
 
     public string HashPassword(string password, string email, byte[] salt)
@@ -22,7 +38,7 @@ public class HashService : IHashService
             byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
             byte[] emailBytes = Encoding.UTF8.GetBytes(email);
             byte[] saltedPassword = new byte[passwordBytes.Length + salt.Length + emailBytes.Length];
-            
+
             Buffer.BlockCopy(salt, 0, saltedPassword, 0, salt.Length);
             Buffer.BlockCopy(passwordBytes, 0, saltedPassword, salt.Length, passwordBytes.Length);
             Buffer.BlockCopy(emailBytes, 0, saltedPassword, passwordBytes.Length + salt.Length, emailBytes.Length);
@@ -50,16 +66,16 @@ public class HashService : IHashService
     }
 
     public bool ComparePassword(string email, string enteredPassword, User user)
-    { 
+    {
         byte[] existingPasswordBytes = Convert.FromBase64String(user.Password!);
         byte[] salt = new byte[16];
-        
+
         Buffer.BlockCopy(existingPasswordBytes, 0, salt, 0, salt.Length);
         Console.WriteLine("ComparePassword:");
         Console.WriteLine("enteredPassword:" + enteredPassword + "user email:" + user.Email + "salt:" + salt);
 
         string generatedPassword = HashPassword(enteredPassword, user.Email!, salt);
-        
+
         Console.WriteLine(generatedPassword);
 
         if (user.Password == generatedPassword)
