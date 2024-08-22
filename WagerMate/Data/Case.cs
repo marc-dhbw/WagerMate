c6646e883c82bbc0d27302d2cd93d57e1b5053e3
anian <anian.seidl@gmail.com>
2024-08-22 11:26:07 +0200

    Added Constructores
    one empty
    one with every Attribute

--- WagerMate/Data/Case.cs
+++ WagerMate/Data/Case.cs
@@ -3,6 +3,16 @@ namespace WagerMate.Data;
 public class Case
 {
     public int Id { get; set; }
-    public int Bet_Id  { get; set; }
-    public string Casetype  { get; set; }
+    public int Bet_Id { get; set; }
+    public string Casetype { get; set; }
+
+    public Case()
+    { }
+
+    public Case(int id, int betId, string casetype)
+    {
+        Id = id;
+        Bet_Id = betId;
+        Casetype = casetype;
+    }
 }
--- WagerMate/Data/User.cs
+++ WagerMate/Data/User.cs
@@ -2,9 +2,19 @@
 
 public class User
 {
-    public int Id { get;set;}
-    public string Email { get;set;}
-    public string Name { get;set;}
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
     public string Password { get; set; }
-    
+
+    public User()
+    { }
+
+    public User(int id, string email, string name, string password)
+    {
+        Id = id;
+        Email = email;
+        Name = name;
+        Password = password;
+    }
 }
